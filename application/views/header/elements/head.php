139117cd4de626751f72d38fe1fcfc0d16f4669f
Darth Vanger <darthvanger@gmail.com>
2014-05-25 11:22:35 +0300

    Uncommented google analytics script

--- application/views/header/elements/head.php
+++ application/views/header/elements/head.php
@@ -2,8 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<!-- Google analytics src - uncomment when uploaded on web -->
-	<!-- <script src="<?=TOOLS?>googleAnalytics.js"></script> -->
+	<script src="<?=TOOLS?>googleAnalytics.js"></script>
 
 	<link href="<?=CSS?>layout.css" rel="stylesheet" type="text/css" />
 	<script src="<?=JS?>config/constants.js" type="text/javascript"></script>
