d18a370fbc949699c39800bad64ef461ce0c2fec
Darth Vanger <darthvanger@gmail.com>
2014-06-07 22:12:02 +0300

    Added xml namespace declaration to header

--- application/views/header/minimal.php
+++ application/views/header/minimal.php
@@ -2,8 +2,10 @@
  -	 Minimal header with doctype declaration, charset, title, and opening <body> tag.
 -->
 
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta charset="UTF-8">
 	<!-- Google analytics src - uncomment when uploaded on web -->
