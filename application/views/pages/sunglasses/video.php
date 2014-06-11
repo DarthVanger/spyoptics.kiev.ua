2b867962039c1ec657b26f0c8475985c9089ed53
Darth Vanger <darthvanger@gmail.com>
2014-06-11 18:59:55 +0300

    Added video page, left navbar for sunglasses page, removed portrait styles

--- application/views/pages/sunglasses/video.php
+++ application/views/pages/sunglasses/video.php
@@ -1,5 +1,7 @@
 <link href="<?=CSS?>pages/video.css" rel="stylesheet" type="text/css" />
 <div id="video-page">
-	<!-- "lazy-src" is for lazy loading: it will be changed to "src" by PageSlidingEffect, when this page is shown -->
-	<iframe class="youtube" src="#" lazy-src="//www.youtube.com/embed/uuXEDOm3PYA" frameborder="0" allowfullscreen></iframe>
+	<div class="iframeContainer">
+		<!-- "lazy-src" is for lazy loading: it will be changed to "src" by PageSlidingEffect, when this page is shown -->
+		<iframe class="youtube" lazy-src="http://www.youtube.com/embed/uuXEDOm3PYA" frameborder="0" allowfullscreen></iframe>
+	</div>
 </div>
