893b4c05c722aeb7ca638ae957f17c59cf4f9a1d
Darth Vanger <darthvanger@gmail.com>
2014-06-21 23:54:06 +0300

    Finishing developing mobile version 0.1

--- application/views/mobile/templates/parts/header.php
+++ application/views/mobile/templates/parts/header.php
@@ -9,7 +9,7 @@
 <head>
 	<meta charset="UTF-8">
     <!-- fixing android divs not 100% width because of zooming -->
-    <meta name="viewport" content="width=device-width" />
+    <meta name="viewport" content="width=1000px" />
 	<!-- Google analytics src - uncomment when uploaded on web -->
 	<!-- <script src="<?=TOOLS?>googleAnalytics.js"></script> -->
 
