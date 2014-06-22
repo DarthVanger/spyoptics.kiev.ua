67fe800b41dc509327e6b984837082742163f578
Darth Vanger <darthvanger@gmail.com>
2014-06-22 21:37:48 +0300

    Added initial scale to fix bug on old iOS

--- application/views/mobile/templates/parts/header.php
+++ application/views/mobile/templates/parts/header.php
@@ -8,7 +8,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width" />
+    <!-- initial-scale is set for iOS landscape mode -->
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 	<!-- Google analytics src - uncomment when uploaded on web -->
 	<!-- <script src="<?=TOOLS?>googleAnalytics.js"></script> -->
 
