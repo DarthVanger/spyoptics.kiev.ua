17b5d67116080df0f4d4780fc84a9133ac372861
Darth Vanger <darthvanger@gmail.com>
2014-06-25 21:48:26 +0300

    Changed mobile header to include pc header

--- application/views/mobile/templates/parts/header.php
+++ application/views/mobile/templates/parts/header.php
@@ -3,7 +3,7 @@
 -->
 
 <!-- load minimal header from pc view -->
-<?php $this->load->view('../../../pc/header/minimal'); ?>
+<?php $this->load->view('pc/header/minimal'); ?>
 
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 
