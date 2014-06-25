043779d4f971ba685a32600e7ddf2e283ce00ed0
Darth Vanger <darthvanger@gmail.com>
2014-06-25 21:22:32 +0300

    Changed mobile header to include pc header

--- application/views/mobile/templates/parts/header.php
+++ application/views/mobile/templates/parts/header.php
@@ -2,30 +2,10 @@
  -	 Header for mobile version of site with doctype declaration, charset, title, <head>, and opening <body> tag.
 -->
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- load minimal header from pc view -->
+<?php $this->load->view('../../../pc/header/minimal'); ?>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-	<meta charset="UTF-8">
-    <!-- initial-scale is set for iOS landscape mode -->
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-	<!-- Google analytics src - uncomment when uploaded on web -->
-	<!-- <script src="<?=TOOLS?>googleAnalytics.js"></script> -->
+<meta name="viewport" content="width=device-width, initial-scale=1" />
 
-	<link rel="stylesheet" href="<?=CSS?>general.css" type="text/css" />
-	<link rel="stylesheet" href="<?=CSS?>mobile/layout.css" type="text/css" />
-	<link rel="stylesheet" href="<?=CSS?>mobile/general.css" type="text/css" />
-
-	<script src="<?=JS?>config/constants.js" type="text/javascript"></script>
-	<!-- jquery & jquery.touch sources -->
-		<script src="<?=JS?>jquery/jquery.min.js"></script>
-		<script src="<?=JS?>jquery/jquery.mobile.touch.min.js"></script>
-		<script src="<?=JS?>jquery/jquery.mobile.touch.patch.js"></script>
-	<!-- ------------ -->
-	<link rel="icon" type="image/png" href="<?=IMG?>favicon.png">
-	<title>Spy Optic Киев</title>
-
-</head>
-<body>
-
+<!-- jquery.touch -->
+<script src="<?=JS?>jquery/jquery.mobile.touch.min.js"></script>
