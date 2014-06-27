a03dab1d7638b01a13ba3314a00ce4cdb207d1b9
Darth Vanger <darthvanger@gmail.com>
2014-06-27 19:35:40 +0300

    Changed sandbox google analytics code to real one

--- assets/tools/googleAnalytics.js
+++ assets/tools/googleAnalytics.js
@@ -5,10 +5,10 @@
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
   // UA-33871645-5 for real statistics
-  //ga('create', 'UA-33871645-5', 'spyoptics.kiev.ua');
+  ga('create', 'UA-33871645-5', 'spyoptics.kiev.ua');
 
   // UA-33871645-6 for sandbox
-  ga('create', 'UA-33871645-6', 'spyoptics.kiev.ua');
+  //ga('create', 'UA-33871645-6', 'spyoptics.kiev.ua');
 
   //for testing on localhost
   //ga('create', 'UA-33871645-6', {
