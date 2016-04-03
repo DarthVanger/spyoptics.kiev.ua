400713c8a1e3c7f6b1c5a03eaac6af204bed68ce
Darth Vanger <darthvanger@gmail.com>
2016-04-03 15:27:29 +0300

    Main page: separated FB and other scial buttons on two sides of page.

--- application/views/pc/footer/social-buttons.php
+++ application/views/pc/footer/social-buttons.php
@@ -3,20 +3,23 @@
   - at the bottom of the page, for main page with sunglusses.
 -->
 <div class="social-buttons-fixed-footer">
-    <div class="social-like-buttons">
-        <div class="like-button-container">
-            <div class="fb-like" data-href="http://spyoptics.kiev.ua/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
-        </div>
-        <div class="like-button-container">
-            <div id="vk_share"></div>
-            <div id="vk_like"></div>
-        </div>
-        <div class="like-button-container">
-            <div class="g-plusone" data-href="http://spyoptics.kiev.ua/"></div>
-        </div>
+
+    <div class="fb-like-button-container fb-like-button-container">
+        <div class="fb-like" data-href="http://spyoptics.kiev.ua/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
     </div>
 
+    <div class="like-button-container">
+        <div id="vk_share"></div>
+        <div id="vk_like"></div>
+    </div>
+
+    <div class="like-button-container">
+        <div class="g-plusone" data-href="http://spyoptics.kiev.ua/"></div>
+    </div>
+
+    <!--
     <div class="loading-container">
         <div id="loading" class="hidden">loading...</div>
     </div>
+    -->
 </div>
