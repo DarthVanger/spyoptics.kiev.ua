4b8b9ca1bdbce5dff544454c4f4bebdd43440856
Darth Vanger <darthvanger@gmail.com>
2016-04-05 21:57:16 +0300

    Fixed bug with footer social buttons covering flexlider's circle-buttons.

--- application/views/pc/footer/social-buttons.php
+++ application/views/pc/footer/social-buttons.php
@@ -2,11 +2,14 @@
   - A block with social buttons, with fixed position
   - at the bottom of the page, for main page with sunglusses.
 -->
-<div class="social-buttons-fixed-footer">
+<div class="footer-social-buttons footer-social-buttons-right-part">
 
     <div class="fb-like-button-container fb-like-button-container">
         <div class="fb-like" data-href="http://spyoptics.kiev.ua/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
     </div>
+</div>
+
+<div class="footer-social-buttons footer-social-buttons-left-part">
 
     <div class="like-button-container">
         <div id="vk_share"></div>
@@ -14,7 +17,8 @@
     </div>
 
     <div class="like-button-container">
-        <div class="g-plusone" data-href="http://spyoptics.kiev.ua/"></div>
+        <div class="g-plusone" data-href="http://spyoptics.kiev.ua/">
+        </div>
     </div>
 
     <!--
