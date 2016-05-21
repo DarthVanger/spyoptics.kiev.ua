4a4d9256900768576e0a004a3c158cf1a16b06e4
Denysiuk Bohdan <kotuchi0203@gmail.com>
2016-05-21 11:47:47 +0200

    test pull request

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
  *
  * You can load different configurations depending on your
  * current environment. Setting the environment also influences
- * things like logging and error reporting.
+ * things like logging and error reporting. anything
  *
  * This can be set to anything, but default usage is:
  *
