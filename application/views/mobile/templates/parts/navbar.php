96be0c19c9fb7cb3bf0bb4eb85a6b8c77730cb84
Denysiuk Bohdan <kotuchi0203@gmail.com>
2016-07-07 15:50:53 +0300

    Changet to circle icons

--- application/views/mobile/templates/parts/navbar.php
+++ application/views/mobile/templates/parts/navbar.php
@@ -3,26 +3,26 @@
 
     <div class="model-switching-navbar">
         <a class="model-switching-button" href="<?=site_url('/#Ken Block Helm')?>" data-model="Ken Block Helm">
-            <img class="for-deselected-img" src="<?=IMG?>pages/sunglasses/kenBlockIconBlackRound.png" />
-			<img class="for-selected-img" src="<?=IMG?>pages/sunglasses/kenBlockIconOrangeRound.png" />
+            <img class="for-deselected-img" src="<?=IMG?>pages/sunglasses/kenBlockIconBlackCircle.png" />
+			<img class="for-selected-img" src="<?=IMG?>pages/sunglasses/kenBlockIconOrangeCircle.png" />
         </a>
         <a class="model-switching-button" href="<?=site_url('/#Flynn')?>" data-model="Flynn">
-            <img class="for-deselected-img" src="<?=IMG?>pages/sunglasses/flynnIconBlackRound.png" />
-			<img class="for-selected-img" src="<?=IMG?>pages/sunglasses/flynnIconOrangeRound.png" />
+            <img class="for-deselected-img" src="<?=IMG?>pages/sunglasses/flynnIconBlackCircle.png" />
+			<img class="for-selected-img" src="<?=IMG?>pages/sunglasses/flynnIconOrangeCircle.png" />
         </a>
         <a class="model-switching-button" href="<?=site_url('/#Touring')?>" data-model="Touring">
-            <img class="for-deselected-img" src="<?=IMG?>pages/sunglasses/touringIconBlackRound.png" />
-			<img class="for-selected-img" src="<?=IMG?>pages/sunglasses/touringIconOrangeRound.png" />
+            <img class="for-deselected-img" src="<?=IMG?>pages/sunglasses/touringIconBlackCircle.png" />
+			<img class="for-selected-img" src="<?=IMG?>pages/sunglasses/touringIconOrangeCircle.png" />
         </a>
 
         <a href="<?=site_url('/shop/order')?>">
             <img src="<?=IMG?>mobile/layout/cartIcon.png" />
         </a>
         <a href="<?=site_url('/shop/showPeoplePhotos')?>">
-            <img src="<?=IMG?>mobile/layout/photosIconBlackRound.png" />
+            <img src="<?=IMG?>mobile/layout/photosIconBlackCircle.png" />
         </a>
         <a href="<?=site_url('/shop/loadSimplePage/contact')?>">
-            <img src="<?=IMG?>mobile/layout/contactIconBlackRound.png" />
+            <img src="<?=IMG?>mobile/layout/contactIconBlackCircle.png" />
         </a>
     </div>
 
