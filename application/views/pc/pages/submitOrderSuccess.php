f65c048c0ba0550f16c8ea918d56fc098e5b71ee
DarthVanger <darthvanger@gmail.com>
2018-11-12 23:23:22 +0200

    Remove store option from vacation message

--- application/views/pc/pages/submitOrderSuccess.php
+++ application/views/pc/pages/submitOrderSuccess.php
@@ -16,13 +16,13 @@
 <link href="<?=CSS.$userDevice?>/pages/submitOrderSuccess.css" rel="stylesheet" type="text/css" />
 <div id="submitOrderSuccess-page" class="page-padding text">
 	<div>
-      <!--
         Спасибо,
         <?php if(isset($post['name'])):?>
             <?=$post['name']?>
         <?php endif;?>
         !
 
+      <!--
         <p>
           Ваш заказ принят и будет отправлен в течении 24 часов по адресу (или на отделение новой почты) <span class="address"><?=$post['addressOrNovaPoshtaOffice']?></span>. 
         </p>
@@ -38,12 +38,6 @@
             <p>
                 В данный момент администрация  интернет магазина в отпуске. Все заказы и сообщения для компании, присланные с с 09.11 до 18.11, будут обработаны 19.11.2018.
             </p>
-            <strong style="font-size: 1.2em">
-            <p>
-                В период отсутсвия администрации сайта, очки можно купить в городе Киеве на нашем складе самовывоза. <br />
-                Контакты склада самовывоза по ссылке: <a href="http://spyoptics.kiev.ua/blog/?p=97">http://spyoptics.kiev.ua/blog/?p=97</a>
-            </p>
-            </strong>
             <p>
                 Приносим извинения за неудобства.
             </p>
